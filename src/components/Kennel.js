1b12d3f2c13560cd208b0359fa14c88e1043f382
Mario Campopiano <mcampopiano92@gmail.com>
2020-11-24 08:39:56 -0600

    created functions and modules to render a nav bar to the DOM which lets you toggle different views

--- src/components/Kennel.js
+++ src/components/Kennel.js
@@ -11,31 +11,12 @@ import { CustomerProvider } from "./customers/CustomerProvider"
 import { CustomerList } from "./customers/CustomerList"
 import { EmployeeProvider } from "./employees/EmployeeProvider"
 import { EmployeeList } from "./employees/EmployeeList"
+import { NavBar } from "./nav/NavBar"
+import { ApplicationViews } from "./ApplicationViews"
 
 export const Kennel = () => (
     <>
-        <h2>Nashville Kennels</h2>
-        <small>Loving care when you're not there.</small>
-        <address>
-            <div>Visit Us at the Nashville North Location</div>
-            <div>500 Puppy Way</div>
-        </address>
-
-        <h2>Animals</h2>
-        <AnimalProvider>
-            <AnimalList />
-        </AnimalProvider>
-        <h2>Employees</h2>
-        <EmployeeProvider>
-            <EmployeeList />
-        </EmployeeProvider>
-        <h2>Locations</h2>
-        <LocationProvider>
-            <LocationList />
-        </LocationProvider>
-        <h2>Customers</h2>
-        <CustomerProvider>
-            <CustomerList />
-        </CustomerProvider>
+        <NavBar />
+        <ApplicationViews />
     </>
 )
