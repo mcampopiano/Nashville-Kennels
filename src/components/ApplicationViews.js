03f23a606bf8b5d67a5f3037fcd67a7db28c7d9d
Mario Campopiano <mcampopiano92@gmail.com>
2020-11-30 15:51:16 -0600

    got the locations to display with amount of animals/employees, and when you click them you see the employee and animal details

--- src/components/ApplicationViews.js
+++ src/components/ApplicationViews.js
@@ -4,23 +4,34 @@ import { LocationProvider } from "./locations/LocationProvider"
 import { AnimalProvider } from "./animal/AnimalProvider"
 import { LocationList } from "./locations/LocationList"
 import { AnimalList } from "./animal/AnimalList"
-import { CustomerProvider } from "./customers/CustomerProvider"
+import { CustomerContext, CustomerProvider } from "./customers/CustomerProvider"
 import { CustomerList } from "./customers/CustomerList"
 import { EmployeeProvider } from "./employees/EmployeeProvider"
 import { EmployeeList } from "./employees/EmployeeList"
 import { EmployeeForm } from "./employees/EmployeeForm"
 import { AnimalForm } from "./animal/AnimalForm"
 import { EmployeeDetail } from "./employees/EmployeeDetail"
+import { LocationDetail } from "./locations/LocationDetail"
 
 export const ApplicationViews = (props) => {
     return (
         <>
-            <LocationProvider>
-                {/* Render the location list when http://localhost:3000/ */}
-                <Route exact path="/">
-                    <LocationList />
-                </Route>
-            </LocationProvider>
+            <EmployeeProvider>
+                <CustomerProvider>
+                    <AnimalProvider>
+                        <LocationProvider>
+                            {/* Render the location list when http://localhost:3000/ */}
+                            <Route exact path="/">
+                                <LocationList />
+                            </Route>
+
+                            <Route path="/locations/:locationId(\d+)" render={
+                                props => <LocationDetail {...props} />
+                            } />
+                        </LocationProvider>
+                    </AnimalProvider>
+                </CustomerProvider>
+            </EmployeeProvider>
 
             <AnimalProvider>
                 <LocationProvider>
